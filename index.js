b46c536a102dc125b436166d3c3c3211d170397f
aunmuhammad1 <f20bseen1m01094@iub.edu.pk>
2024-08-09 21:13:17 +0500

    reset

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 
-
-// Define allowed domains for CORS
-const allowedDomains = ['https://127.0.0.1:3000', 'https://skills-test-frontend.vercel.app/'];
-
-// CORS configuration
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedDomains.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-// Apply CORS middleware
-app.use(cors(corsOptions));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
