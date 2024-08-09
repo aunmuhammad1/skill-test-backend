6ccd30efb33579d7236d340ec8301c193e3d3e47
aunmuhammad1 <f20bseen1m01094@iub.edu.pk>
2024-08-09 20:35:26 +0500

    speacific domain

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+
+// Define allowed domains for CORS
+const allowedDomains = ['https://127.0.0.1:3000', 'https://skills-test-frontend.vercel.app/'];
+
+// CORS configuration
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedDomains.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.json({ message: 'This is CORS-enabled for specific domains.' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+// Start the server
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
